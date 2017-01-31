3a38435ddccdf039dfebfb7cb91e5c2f347dcbab
david-babunashvili <babunashvilidato@gmail.com>
2017-01-31 22:33:39 +0400

    Added routes in stack

---
+++ src/containers/Wishlist.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+/**
+ * Create Wishlist Container
+ */
+ class Wishlist extends Component{
+ 	render(){
+ 		return (
+ 			<section className="section">
+ 			<div className="container">
+ 			<div className="heading">
+ 			<h1 className="title">My Wishlist</h1>
+ 			</div>
+ 			 Wishlist...
+ 			 </div>
+ 			</section>
+ 			)
+    }
+}
+/**
+ * Insert Props Into Component
+ */
+ const stateProps = (state) => {
+ 	return { 
+
+ 	}
+ }
+
+ export default connect(stateProps)(Wishlist)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
+import About from './containers/About'
+import Cart from './containers/Cart'
+import Wishlist from './containers/Wishlist'
+import ProductList from './containers/ProductList'
 import { Provider } from 'react-redux'
 import store from './store'
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router'
 /**
  * Render App
  */
 ReactDOM.render(
    <Provider store={store}>
-	  	<App/>
+     <Router history={hashHistory}> 
+	     <Route path="/" component={App} >
+	         <IndexRoute component={ProductList} />
+             <Route path="about" component={About} />
+             <Route path="cart" component={Cart} />
+             <Route path="wishlist" component={Wishlist} />
+	     </Route>
+	 </Router>  	
 	</Provider>,
   document.getElementById('root')
 );
